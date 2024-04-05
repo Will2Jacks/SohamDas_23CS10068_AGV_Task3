f3b3d00b1854f80390e25dc24880398f037e3230
Soham Das <140496374+Will2Jacks@users.noreply.github.com>
2024-04-05 23:14:04 +0530

    Update computeDictionary.py

--- computeDictionary.py
+++ computeDictionary.py
@@ -9,10 +9,10 @@ train_imagenames = meta['train_imagenames']
 # -----fill in your implementation here --------
 
 random_dict=get_dictionary(train_imagenames,50,100,'Random')
-pickle.dump(random_dict,'../data/dictionaryRandom.pkl')
+pickle.dump(random_dict,open('../data/dictionaryRandom.pkl', 'wb'))
 
-harris_dict=get_dictionary(train_imagenames,50,100,'Random')
-pickle.dump(random_dict,'../data/dictionaryHarris.pkl')
+harris_dict=get_dictionary(train_imagenames,50,100,'Harris')
+pickle.dump(random_dict,open('../data/dictionaryHarris.pkl', 'wb'))
 # ----------------------------------------------
 
 
