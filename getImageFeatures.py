7b1d718e67a1fcf658c74ade1664247888adfbe6
Soham Das <140496374+Will2Jacks@users.noreply.github.com>
2024-04-05 23:27:31 +0530

    Update getImageFeatures.py

--- getImageFeatures.py
+++ getImageFeatures.py
@@ -5,12 +5,8 @@ def get_image_features(wordMap, dictionarySize):
 
     # -----fill in your implementation here --------
 
-    h=[]
-    for i in range(dictionarySize):
-        h.append(0)
-    h=np.array(h)
-    for val in range(dictionarySize):
-        h[val]=np.count(wordMap,val)
+    h = np.bincount(wordMap.ravel(), minlength=dictionarySize)
+    return h
     # ----------------------------------------------
     
     return h
